af6defedf66e6b8f0cd0428b43c1a0571c343c54
handongxun <handongxun@dcloud.io>
2020-04-08 11:56:51 +0800

    update button style

--- src/pages/button/button.vue
+++ src/pages/button/button.vue
@@ -1,55 +1,66 @@
 <template>
-    <view class="content">
-        <view class="uni-padding">
-            <button class="button" type="primary">页面主操作 Normal</button>
-            <!-- <button class="button" type="primary" :loading="loading">页面主操作 Loading</button> -->
-            <button class="button" type="primary" disabled="true">页面主操作 Disabled</button>
+  <view class="content">
+    <button class="button" type="primary">页面主操作 Normal</button>
+    <!-- <button class="button" type="primary" :loading="loading">页面主操作 Loading</button> -->
+    <button class="button" type="primary" disabled="true">页面主操作 Disabled</button>
 
-            <button class="button" type="default">页面次要操作 Normal</button>
-            <button class="button" type="default" disabled="true">页面次要操作 Disabled</button>
+    <button class="button" type="default">页面次要操作 Normal</button>
+    <button class="button" type="default" disabled="true">页面次要操作 Disabled</button>
 
-            <button class="button" type="warn">警告类操作 Normal</button>
-            <button class="button" type="warn" disabled="true">警告类操作 Disabled</button>
+    <button class="button" type="warn">警告类操作 Normal</button>
+    <button class="button" type="warn" disabled="true">警告类操作 Disabled</button>
 
-            <view class="button-sp-area1">
-                <button type="primary" plain="true">按钮</button>
-                <button type="primary" disabled="true" plain="true">不可点击的按钮</button>
+    <view class="button-sp-area">
+      <button class="button" type="primary" plain="true">按钮</button>
+      <button class="button" type="primary" disabled="true" plain="true">不可点击的按钮</button>
 
-                <button type="default" plain="true">按钮</button>
-                <button type="default" disabled="true" plain="true">按钮</button>
-
-                <button class="mini-btn" type="primary" size="mini">按钮</button>
-                <button class="mini-btn" type="default" size="mini">按钮</button>
-                <button class="mini-btn" type="warn" size="mini">按钮</button>
-            </view>
-        </view>
+      <button class="button" type="default" plain="true">按钮</button>
+      <button class="button" type="default" disabled="true" plain="true">按钮</button>
     </view>
+    <view class="button-sp-area button-mini-area">
+      <button class="mini-btn" type="primary" size="mini">按钮</button>
+      <button class="mini-btn" type="default" size="mini">按钮</button>
+      <button class="mini-btn" type="warn" size="mini">按钮</button>
+    </view>
+  </view>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                title: 'Hello'
-            }
-        },
-        onLoad() {},
-        methods: {
+  export default {
+    data() {
+      return {
+        title: 'Hello'
+      }
+    },
+    onLoad() {},
+    methods: {
 
-        }
     }
+  }
 </script>
 
 <style>
-    .content {}
+  .content {
+    padding-left: 30px;
+    padding-right: 30px;
+    padding-bottom: 40px;
+  }
 
-    .button {
-        margin-top: 30rpx;
-        margin-bottom: 30rpx;
-    }
+  .button {
+    margin-bottom: 30rpx;
+  }
 
-    .uni-padding {
-        padding-left: 15px;
-        padding-right: 15px;
-    }
+  .button-sp-area {
+    width: 80%;
+    margin: auto;
+  }
+
+  .button-mini-area {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .mini-btn {
+    margin: 4px;
+  }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -27,26 +27,7 @@
   .content {
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    padding: 15px;
   }
 
-  .logo {
-    height: 200rpx;
-    width: 200rpx;
-    margin-top: 200rpx;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 50rpx;
-  }
-
-  .text-area {
-    display: flex;
-    justify-content: center;
-  }
-
-  .title {
-    font-size: 36rpx;
-    color: #8f8f94;
-  }
 </style>
